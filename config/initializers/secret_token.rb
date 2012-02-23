4c06d0100c22342a45cb0bba468a61ede5159a70
z8888q <z8888q@gmail.com>
2012-02-23 22:26:18 +0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AndRuby::Application.config.secret_token = '6cdc1bc6bc69f2869721336a58f84440b7c1bb3a1fd9e8c77a52c10c620e05e26c926ea7c9443320091eca5722e762383cdc751c3a10f8cac7c62257f13c2ff6'
